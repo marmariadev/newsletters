ac4a23a3042fdc813f8b9055527b7e111c49be66
marmariadev <marmariadev@gmail.com>
2024-02-27 18:25:59 -0300

    Add morgan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 // /src/app.js
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
