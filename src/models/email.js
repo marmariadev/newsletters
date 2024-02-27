fba3e7f6db14ad503620eac01e777802b782b657
marmariadev <marmariadev@gmail.com>
2024-02-27 19:08:26 -0300

    Add email model

---
+++ src/models/email.js
@@ -0,0 +1,16 @@
+// /src/models/email.js
+const mongoose = require('mongoose');
+
+const emailSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Email', emailSchema);
